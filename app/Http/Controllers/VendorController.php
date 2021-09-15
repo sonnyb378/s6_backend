2598a1043941e3ff9ec39b912fd29cf492788df2
sonnybaga <sonnybaga@gmail.com>
2021-09-15 13:21:51 -0700

    added helper function

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Vendor;
 use App\Models\Product;
+use Helper;
 
 class VendorController extends Controller
 {
@@ -17,84 +18,33 @@ class VendorController extends Controller
         return response()->json(["message" => "Vendor does not exist"]);
     }
 
-    
-
     public function orders($vendor_id) {
         $vendor = Vendor::find($vendor_id);
         if ($vendor) {   
-            $index_found = null;
-            if (count($vendor->orders) > 0) {
-                $orders = [];
-                $print_line_items = [];
-                $temp = [];
-                foreach($vendor->orders as $order) {
-                    $line_items = [];
-                    $imagepath = Product::find($order->pivot->product_id)->creative->image_url;
-                    
-                    array_push($line_items, [
-                        "external_order_line_item_id" => $order->pivot->id,
-                        "product_id" => $order->pivot->product_id,
-                        "quantity" => $order->pivot->quantity,
-                        "image_url" => $imagepath
-                    ]); 
-                    
-                    if (count($orders) > 0) {                        
-                        $index_found = array_search($order->id, array_values($orders));
-                        $order_id_exists = in_array($order->id, $orders[$index_found]);
-                        if ($order_id_exists) {
-                            $temp = $orders[$index_found]['print_line_items'];
-                            array_splice($orders, $index_found, 1);
-                        }
-                    }
-
-                    // $order_id_exists = array_filter($orders, fn($k) => $k["external_order_id"] == $order->id);
-                    // if ($order_id_exists) {                       
-                    //     $index_found = array_search($order->id, array_values($order_id_exists[0]));
-                    //     $temp = $order_id_exists[0]['print_line_items'];                        
-                    //     if ($index_found !== null) {
-                    //         array_splice($orders, $index_found, 1);
-                    //     }
-                    // }
-
-                    $print_line_items = array_merge($line_items, $temp);                    
-                    
-                    array_push($orders, [
-                        "external_order_id" => $order->id,
-                        "buyer_first_name" => $order->first_name,
-                        "buyer_last_name" => $order->last_name,
-                        "buyer_shipping_address_1" => $order->address_1,
-                        "buyer_shipping_address_2" => $order->address_2,
-                        "buyer_shipping_city" => $order->city,
-                        "buyer_shipping_state" => $order->state,
-                        "buyer_shipping_postal" => $order->postal_code,
-                        "buyer_shipping_country" => $order->country,
-                        "print_line_items" => $print_line_items
-                    ]);
-
-                    
-                    
-                }
-                
             
-            } else {
+            $helper = new Helper;
+            $orders = $helper->render($vendor);
+            
+            if (!$orders) {
                 return response()->json(["message" => "No orders exist for the vendor"]);
+            } else {
+                $data = [
+                    "orders" => $orders,
+                ];
+                
+                switch ($vendor->response[0]->name) {
+                    case 'json':
+                        return response()->json([
+                            "data" => $data
+                        ]);
+                        break;
+                    case 'xml':
+                        return response()->view('xml', ['data' => $data])->header("Content-Type", 'xml');
+                        break;
+                }
             }
 
-
-            $data = [
-                "orders" => $orders,
-            ];
-
-            switch ($vendor->response[0]->name) {
-                case 'json':
-                    return response()->json([
-                        "data" => $data
-                    ]);
-                    break;
-                case 'xml':
-                    return response()->view('xml', ['data' => $data])->header("Content-Type", 'xml');
-                    break;
-            }
+            
             
         
         }
